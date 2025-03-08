de0cf7667aa63d31285cf1e2a1fcc9a0cf7136de
K4mD4m <KamyszekDamian@outlook.com>
2025-03-08 12:06:01 +0100

    Movie card- fixed

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -14,31 +14,31 @@ const MovieCard: React.FC<MovieCardProps> = ({
   overview,
 }) => {
   return (
-    <div className="group relative rounded-lg overflow-hidden bg-gray-800 bg-opacity-70 hover:bg-opacity-80 shadow-xl transition duration-500 w-full lg:w-[90%] max-w-72 lg:max-w-xs mx-auto">
+    <div className="group relative rounded-lg overflow-hidden bg-gray-800 bg-opacity-70 hover:bg-opacity-80 shadow-xl transition duration-500 w-full max-w-64 sm:max-w-72 lg:max-w-xs mx-auto">
       {/* Plakat filmu */}
-      <div className="relative w-full h-72">
+      <div className="relative w-full aspect-[2/3]">
         <img
           src={`https://image.tmdb.org/t/p/w500${poster_path}`}
           alt={title}
-          className="object-fill w-full h-full group-hover:scale-110 transition-all duration-300"
+          className="object-fill w-full h-full"
         />
       </div>
 
       {/* Tytuł */}
-      <div className="p-4 text-white">
-        <h3 className="text-lg font-semibold text-indigo-400 group-hover:text-white transition duration-200 line-clamp-1">
+      <div className="p-3 sm:p-4 text-white">
+        <h3 className="text-base sm:text-lg font-semibold text-indigo-400 group-hover:text-white transition duration-200 line-clamp-1">
           {title}
         </h3>
 
         {/* Opis */}
-        <p className="text-sm mt-2 text-gray-300 group-hover:text-gray-200 transition duration-200 line-clamp-3">
+        <p className="text-xs sm:text-sm mt-1 sm:mt-2 text-gray-300 group-hover:text-gray-200 transition duration-200 line-clamp-3">
           {overview}
         </p>
 
         {/* Przekierowanie do szczegółów filmu po id */}
         <Link
           to={`/movie/${id}`}
-          className="text-indigo-400 hover:text-indigo-300 mt-3 inline-block transition font-semibold"
+          className="text-indigo-400 hover:text-indigo-300 mt-2 sm:mt-3 inline-block transition font-semibold text-sm sm:text-base"
         >
           View Details →
         </Link>
